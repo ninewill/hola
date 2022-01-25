91686936a07e7eac38e9e06d2a3511124dfb9029
ninewill <wtony1239271@gmail.com>
2022-01-25 10:57:29 +0800

    過渡版調整

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,10 @@ module.exports = {
     screens: {
       sm: "640px",
       md: "768px",
-      lg: "1024px",
+      lg: "1025px",
       xl: "1280px",
-      "2xl": "1536px",
+      "2xl": "1440px",
+			"3xl": "1660px",
     },
     colors: {
       transparent: "transparent",
